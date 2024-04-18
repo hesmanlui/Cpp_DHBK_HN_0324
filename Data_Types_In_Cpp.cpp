37b1e8fbfa30991e007e7174a985ac57571cc884
hesmanlui <hesmanlui@outlook.com>
2024-04-18 23:35:45 +0700

    11:35PM 18-04-2024

--- Data_Types_In_Cpp.cpp
+++ Data_Types_In_Cpp.cpp
@@ -23,6 +23,7 @@ int main()
     cout << "---------------------------------" << endl;
     cout << "Kich thuoc cua long la: " << sizeof(long) << endl;
     cout << "*********************************"<< endl;
+    cout << "---------------------------------" << endl;
     cout << "Kich thuoc cua float la: " << sizeof(float) << endl;
     cout << "*********************************"<< endl;
     cout << "Kich thuoc cua double la: " << sizeof(double) << endl;
