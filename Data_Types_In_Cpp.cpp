34fb5891f59a4e64f4cbb9c5496d010dd5a56303
hesmanlui <hesmanlui@outlook.com>
2024-05-10 22:35:40 +0700

    10:36PM 10-05-2024

--- Data_Types_In_Cpp.cpp
+++ Data_Types_In_Cpp.cpp
@@ -6,54 +6,53 @@ int main()
     cout << "TRA VE KICH THUOC CUA CAC KIEU DU LIEU TRONG C++" << endl;
     cout << "++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 1:
     cout << "Kich thuoc cua unsigned char la: " << sizeof(unsigned char) << endl;
     cout << "*********************************" << endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 2:
     cout << "Kich thuoc cua char la: " << sizeof(char) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 3:
     cout << "Kich thuoc cua unsigned int la: " << sizeof(unsigned int) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 4:
     cout << "Kich thuoc cua short int la: " << sizeof(short int) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 5:
     cout << "Kich thuoc cua int la: " << sizeof(int) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 6:
     cout << "Kich thuoc cua unsigned long la: " << sizeof(unsigned long) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 7:
     cout << "Kich thuoc cua long la: " << sizeof(long) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 8:
     cout << "Kich thuoc cua float la: " << sizeof(float) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 9:
     cout << "Kich thuoc cua double la: " << sizeof(double) << endl;
     cout << "*********************************"<< endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
-    // Ghi chú:
+    // Ghi chú 10:
     cout << "Kich thuoc cua long double la: "<< sizeof(long double) << endl;
-    cout << "---------------------------------" << endl;
     cout << "---------------------------------" << endl;
     cout << "\\\\" << endl;
     cout << "End -----------------------------" << endl;
